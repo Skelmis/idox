8d3e66b3cc72ab876f569dca16def73541a6155e
skelmis <skelmis.craft@gmail.com>
2024-10-12 13:27:28 +1300

    feat: move to poetry

--- idox/__init__.py
+++ idox/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 import logging
 from collections import namedtuple
@@ -9,7 +9,7 @@ from idox.sequences import SequenceT, NumericSequence, FileSequence
 from idox.idox import Idox
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=1, micro=0, releaselevel="beta", serial=0)
+version_info = VersionInfo(major=1, minor=3, micro=0, releaselevel="beta", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = (
